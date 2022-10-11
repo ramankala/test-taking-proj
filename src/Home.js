e4fc14a087e4096c758f1efbbdd24e8824c301f2
Raman <rs.kala@hotmail.com>
2022-10-11 18:06:41 -0400

    Complete company section of landing page

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,15 @@ const Home = () => {
                 <div>
                     <div>This website is awesome</div>
                     <div>This website has some subtext that goes here under the main title.  It's a smaller font and the colour is a lower contrast</div>
-                    <div>Sign me up</div>
+                    <div className = 'sign-up-btn'>Sign me up</div>
                 </div>
                 <div>placeholder for img</div>
             </div>
             <div className = 'testBox'>
-
+                stuff
             </div>
             <div className = 'quoteBox'>
-
+                stuff
             </div>
         </div>
     )
